f05439e02d5f59dc533cb1f9344be57b07bcfb6a
sena <sena0716j@gmail.com>
2026-08-04 21:32:27 +0900

    add more stuck glitches

--- data-inverted-camera-glitch.js
+++ data-inverted-camera-glitch.js
@@ -3,8 +3,8 @@ const invertedCameraPages = {
         title: "Inverted Camera Glitch",
         html: `
             <h1>Inverted Camera Glitch</h1>
-            <p>Inside the rock wall in Riverside Park, there's a strip of ground along the edge that curves all the way around. If you go there, your character won't fall, but the camera will flip upside-down.</p>
             <iframe width="480" height="270" src="https://www.youtube.com/embed/sPsxQQuUoE0?si=OUziKM85Eg-rxax7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <p>Inside the rock wall in Riverside Park, there's a strip of ground along the edge that curves all the way around. If you go there using a <a href="#wall-cross">Wall Cross</a>, your character won't fall, but the camera will flip upside-down.</p>
         `
     }
 };
--- data-stuck-glitch.js
+++ data-stuck-glitch.js
@@ -10,9 +10,19 @@ const stuckGlitchPages = {
                 <li><a href="#stuck-athens">Athens Dash</a></li>
                 <li><a href="#stuck-twisted">Twisted Mansion</a></li>
                 <li><a href="#stuck-ros">Los Angeles Laps</a></li>
-                <li><a href="#stuck-merimeri">Merry Mountain</a></li>
+                <li><a href="#stuck-merry-mountain">Merry Mountain</a></li>
                 <li><a href="#stuck-misaki">Dolphin Shoals</a></li>
                 <li><a href="#stuck-ribon">Ribbon Road</a></li>
+
+                <li><a href="#stuck-tikutaku">Tick-Tock Clock</a></li>
+                <li><a href="#stuck-sunshine">Sunshine Airport</a></li>
+                <li><a href="#stuck-coconut">Coconut Mall</a></li>
+                <li><a href="#stuck-dezikuru">Daisy Cruiser</a></li>
+                <li><a href="#stuck-gura">Grumble Volcano</a></li>
+                <li><a href="#stuck-jungle">DK Jungle</a></li>
+                <li><a href="#stuck-kurosu">DK Summit</a></li>
+                <li><a href="#stuck-nin">Ninja Hideaway</a></li>
+                <li><a href="#stuck-pakusin">Piranha Plant Cove</a></li>
             </ul>
         `
     },
@@ -52,12 +62,12 @@ const stuckGlitchPages = {
             <p>You can ride a glider and get wedged by timing it right to hit objects that move up and down. The character's movement after getting stuck is pretty unique.</p>
         `
     },
-    "stuck-merimeri": {
+    "stuck-merry-mountain": {
         title: "Merry Mountain - Stuck Glitch",
         html: `
             <h1>Merry Mountain</h1>
             <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
-            <video src="videos/meri.mp4" controls width="600"></video>
+            <video src="videos/stuck-merry-mountain.mp4" controls width="600"></video>
             <p>If you use a mushroom to jump from the bridge at the beginning of the course toward the wall behind it, you can get stuck.</p>
         `
     },
@@ -78,5 +88,86 @@ const stuckGlitchPages = {
             <video src="videos/ribon.mp4" controls width="600"></video>
             <p>You can get stuck if you hit the cart's handle using the waves on the floor.</p>
         `
-    }
+    },
+    "stuck-tikutaku": {
+        title: "Tick-Tock Clock - Stuck Glitch",
+        html: `
+            <h1>Tick-Tock Clock</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/tikutaku.mp4" controls width="600"></video>
+            <p>Use a Mushroom to jump when the gear rotates forward.</p>
+        `
+    },
+    "stuck-sunshine": {
+        title: "Sunshine Airport - Stuck Glitch",
+        html: `
+            <h1>Sunshine Airport</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/sunshine.mp4" controls width="600"></video>
+            <p>Use an <a href="#edge-clip">Edge Clip</a> behind the airplane.</p>
+        `
+    },
+    "stuck-coconut": {
+        title: "Coconut Mall - Stuck Glitch",
+        html: `
+            <h1>Coconut Mall</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/coconut.mp4" controls width="600"></video>
+            <p>You can get wedged between the rocks.</p>
+        `
+    },
+    "stuck-dezikuru": {
+        title: "Daisy Cruiser - Stuck Glitch",
+        html: `
+            <h1>Daisy Cruiser</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/dezikuru.mp4" controls width="600"></video>
+            <p>While the table is moving, hit yourself with a Green Shell to get stuck.</p>
+        `
+    },
+    "stuck-gura": {
+        title: "Grumble Volcano - Stuck Glitch",
+        html: `
+            <h1>Grumble Volcano</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/gura.mp4" controls width="600"></video>
+            <p>Use a Mushroom near the wall to trigger a high jump.</p>
+        `
+    },
+    "stuck-jungle": {
+        title: "DK Jungle - Stuck Glitch",
+        html: `
+            <h1>DK Jungle</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/jungle.mp4" controls width="600"></video>
+            <p>Use a Mushroom to land on the rock.</p>
+        `
+    },
+    "stuck-kurosu": {
+        title: "DK Summit - Stuck Glitch",
+        html: `
+            <h1>DK Summit</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/kurosu.mp4" controls width="600"></video>
+            <p>Ride onto the left side of the half-pipe with a bit of momentum to get stuck.</p>
+        `
+    },
+    "stuck-nin": {
+        title: "Ninja Hideaway - Stuck Glitch",
+        html: `
+            <h1>Ninja Hideaway</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/nin.mp4" controls width="600"></video>
+            <p>In 200cc, while the ground is rising, use a Mushroom to land on top of the sign.</p>
+        `
+    },
+    "stuck-pakusin": {
+        title: "Piranha Plant Cove - Stuck Glitch",
+        html: `
+            <h1>Piranha Plant Cove</h1>
+            <p><a href="#stuck-glitch" class="back-link">Stuck Glitch List</a></p>
+            <video src="videos/pakusin.mp4" controls width="600"></video>
+            <p>Use the water current to land on the anchor and get stuck.</p>
+        `
+    },
 };
