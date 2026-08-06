bb626c64c5eed26b9b21d8c906b9c15c3b7632e5
sena <sena0716j@gmail.com>
2026-08-06 19:43:25 +0900

    added a link into home page

--- data-home.js
+++ data-home.js
@@ -5,8 +5,7 @@ const homePage = {
             <div class="home-page">
                 <img class="home-logo" src="images/mk8dxlogo.png" alt="Logo">
                 <h1>Mario Kart 8 Deluxe Glitch Gallery</h1>
-                <p>This page is a site that shows off various bugs that exist in Mario Kart 8 Deluxe.</p>
-            </div>
+                <p>This page is a site that shows off various bugs that exist in Mario Kart 8 Deluxe. <a href="javascript:void(0)" onclick="event.stopPropagation(); document.getElementById('menu-toggle').click();">View the Glitch List</a></p>            </div>
         `
     }
 };
